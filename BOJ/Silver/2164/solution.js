0f5194b73c625ebb2d3a965771472f7d8adb38a7
ssk7808 <ssk7808@naver.com>
2024-06-14 19:55:54 +0900

    [BOJ] / silver 4 / 30 min

---
+++ BOJ/Silver/2164/solution.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const root =
+  process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
+const input = fs.readFileSync(root, 'utf8').toString().trim();
+
+const N = Number(input);
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+    }
+    this.tail = newNode;
+    this.length++;
+
+    return newNode;
+  }
+  getHead() {
+    return this.head.val;
+  }
+  removeHead() {
+    this.head = this.head.next;
+    this.head.prev = null;
+    this.length--;
+  }
+  getLength() {
+    return this.length;
+  }
+}
+
+const cards = new LinkedList();
+
+for (let i = 1; i < N + 1; i++) {
+  cards.push(i);
+}
+while (cards.getLength() > 1) {
+  cards.removeHead();
+  cards.push(cards.getHead());
+  cards.removeHead();
+}
+console.log(cards.getHead());
